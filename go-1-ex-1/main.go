9674a2c63566ceaf64d90b7803468153ca8402ec
Nils Steiner <Nils_Steiner@sluz.ch>
2024-11-21 15:47:14 +0100

    ex1-solved

--- go-1-ex-1/main.go
+++ go-1-ex-1/main.go
@@ -2,8 +2,20 @@ package main
 
 import "fmt"
 
+// Deklaration
+var firstName string = "Nils"
+var lastName string = "Steiner"
+var dayOfBirth int = 3
+var monthOfBirth int = 12
+var yearOfBirth int = 2007
+var numberOfSiblings int = 0
+var heightInMeters float64 = 1.90
+var zodiacSign rune = 'S'
+
 func main() {
 	// TODO: Declare and initialize the variables being used in the output!
+
+	// Ausgabe
 	fmt.Printf("Vor- und Nachname: %s %s\n", firstName, lastName)
 	fmt.Printf("Geburtsdatum: %d.%d.%d\n", dayOfBirth, monthOfBirth, yearOfBirth)
 	fmt.Printf("Anzahl Geschwister: %d\n", numberOfSiblings)
