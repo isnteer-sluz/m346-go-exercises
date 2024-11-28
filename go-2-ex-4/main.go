541f449997d90f7ea4f70b20f999ee93a5023490
nilssteiner123 <steiner.nils@durango.ch>
2024-11-28 16:25:57 +0100

    ex 4 Lösung

--- go-2-ex-4/main.go
+++ go-2-ex-4/main.go
@@ -1,18 +1,18 @@
 package main
 
 func main() {
-	// TODO: declare a type for Student (with first and last name)
+	// declare a type for Student (with first and last name)
 	type Student struct {
 		FirstName string
 		LastName  string
 	}
 	
-	// TODO: declare a type for Class (consisting of multiple students)
+	// declare a type for Class (consisting of multiple students)
 	type Class struct {
 		Students []Student
 	}
 
-	// TODO: declare a map of modules being attended by multiple classes
+	// declare a map of modules being attended by multiple classes
 	modules := map[int]Class{
 		104: {
 			Students: []Student{
@@ -34,7 +34,10 @@ func main() {
 		}
 	}
 
-	// TODO: output everything using fmt.Println()
+// TODO: Aufgabe muss ich nochmals überarbeiten weil ich ein Fehler gemacht habe
+
+
+	// output everything using fmt.Println()
 	fmt.Println("Modul 104:", modules[104])
 	fmt.Println("Modul 117:", modules[117])
 	fmt.Println("Modul 346:", modules[346])
