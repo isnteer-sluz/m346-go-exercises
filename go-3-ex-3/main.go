09a485234987374912d7d5cec9c77700d956c938
nilssteiner123 <steiner.nils@durango.ch>
2024-12-05 15:59:54 +0100

    go-3-ex-3 Complete ✅

--- go-3-ex-3/main.go
+++ go-3-ex-3/main.go
@@ -6,5 +6,16 @@ const (
 )
 
 func main() {
-	// TODO: Implement FizzBuzz using a for loop from Lower to Upper.
+	for Lower := 1; Lower < Upper; Lower++ {
+		if Lower % 3 == 0 && Lower % 5 == 0 {
+			fmt.Printf("FizzBuzz ")
+		} else if Lower % 3 == 0 {
+			fmt.Printf("Fizz ")
+		} else if Lower % 5 == 0 {
+			fmt.Printf("Buzz ")
+		} else {
+			fmt.Printf("%d ", Lower)
+		}
+	}
+	fmt.Println()
 }
