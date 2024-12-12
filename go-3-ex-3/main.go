0846ed934af1068f522596c09f647a805dd2a4f0
nilssteiner123 <steiner.nils@durango.ch>
2024-12-12 15:30:23 +0100

    go-ex-4-ex-1 Complete ✅

--- go-3-ex-3/main.go
+++ go-3-ex-3/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 const (
 	Lower = 1
 	Upper = 30
@@ -7,15 +9,15 @@ const (
 
 func main() {
 	for Lower := 1; Lower < Upper; Lower++ {
-		if Lower % 3 == 0 && Lower % 5 == 0 {
+		if Lower%3 == 0 && Lower%5 == 0 {
 			fmt.Printf("FizzBuzz ")
-		} else if Lower % 3 == 0 {
+		} else if Lower%3 == 0 {
 			fmt.Printf("Fizz ")
-		} else if Lower % 5 == 0 {
+		} else if Lower%5 == 0 {
 			fmt.Printf("Buzz ")
 		} else {
 			fmt.Printf("%d ", Lower)
 		}
+		fmt.Println()
 	}
-	fmt.Println()
 }
--- go-4-ex-1/main.go
+++ go-4-ex-1/main.go
@@ -1,7 +1,21 @@
 package main
 
+import "fmt"
+
 // TODO: implement the function computeGrade
+func computeGrade(gotPoints int, maxPoints int) float64 {
+
+	var result float64 = ((float64(gotPoints) / float64(maxPoints)) * 5) + 1
+
+	if result < 1 {
+		return 1
+	} else if result > 6 {
+		return 6
+	} else {
+		return result
+	}
+}
 
 func main() {
-	// TODO: call the function computeGrade
+	fmt.Println(computeGrade(0, 100))
 }
