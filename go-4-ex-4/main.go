97708ccf39e545c643fce3face1a192b8f05c717
nilssteiner123 <steiner.nils@durango.ch>
2024-12-12 16:26:08 +0100

    go-4-ex-4 Complete ✅

--- go-4-ex-4/main.go
+++ go-4-ex-4/main.go
@@ -1,9 +1,22 @@
 package main
 
 // TODO: implement the function convertCelsiusToFahrenheit
+func convertCelsiusToFahrenheit(celsius float64) float64 {
+	return (celsius - 32) * (5/9)
+}
 // TODO: implement the function convertFahrenheitToCelsius
+func convertFahrenheitToCelsius(FahrenheitToCelsius float64) float64 {
+	return (celsius * (9/5) + 32)
+}
 
 func main() {
 	// TODO: call the function convertCelsiusToFahrenheit
+	fmt.Println(convertCelsiusToFahrenheit(30))
+	fmt.Println(convertCelsiusToFahrenheit(40))
+	fmt.Println(convertCelsiusToFahrenheit(10))
+
 	// TODO: call the function convertFahrenheitToCelsius
+	fmt.Println(convertFahrenheitToCelsius)
+	fmt.Println(convertFahrenheitToCelsius)
+	fmt.Println(convertFahrenheitToCelsius)
 }
