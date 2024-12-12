0bfbb9ab2b51052009da4272f4935e854937b39e
nilssteiner123 <steiner.nils@durango.ch>
2024-12-12 15:39:53 +0100

    go-4-ex-2 Complete ✅

--- go-4-ex-2/main.go
+++ go-4-ex-2/main.go
@@ -8,4 +8,8 @@ func computeHypotenuse(a float64, b float64) float64 {
 func main() {
 	// TODO: call the function computeHypotenuse
 	// Kannst du mir ein testing für die Funktion schreiben mit kommentaren was das resultat
+	fmt.Println(computeHypotenuse(3, 4)) // 5
+	fmt.Println(computeHypotenuse(5, 12)) // 13
+	fmt.Println(computeHypotenuse(8, 15)) // 17
+	fmt.Println(computeHypotenuse(7, 24)) // 25
 }
